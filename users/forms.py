06b8cbef486270f99afb260459fa959ee4a00ff7
Your Name <abhisespoudyal@gmail.com>
2020-05-25 20:53:12 +0545

    add gitignore

--- users/forms.py
+++ users/forms.py
@@ -27,4 +27,4 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model=User
         fields=['username','email','password1','password2',
-        'country','age','height','weight']
+        'country','age','height','weight','workout']
